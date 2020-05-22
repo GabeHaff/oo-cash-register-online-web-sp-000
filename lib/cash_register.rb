9e18d3602d7d9341e349e7fa8a4580f008578234
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-05-22 06:23:23 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,7 +8,7 @@ class CashRegister
 end 
 
 def add_item(title, price, quantity=1) #'title' reverted from 'items'; #:title attr_accessor removed
-self.transaction = [title, price, quantity]
+@transaction = [title, price, quantity]
 quantity.to_i.times do 
  self.items << self.transaction #changed from items
 end 
