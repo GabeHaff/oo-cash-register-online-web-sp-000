75000f2434a86be9f0b1f01668f39e3264f68443
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-05-22 06:09:06 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,81 @@
+class CashRegister
+  attr_accessor :discount, :total, :items, :price, :transaction
+  
+  def initialize(discount=nil)
+  self.total = 0 
+  self.discount = discount if discount !=nil
+  self.items = [ ]
+end 
 
+def add_item(title, price, quantity=1) #'title' reverted from 'items'; #:title attr_accessor removed
+self.transaction = [title, price, quantity]
+@total += price*quantity
+quantity.to_i.times do 
+ self.items << self.transaction #changed from items
+end 
+end
+
+def apply_discount
+       if discount 
+         discount = (100.0 - @discount).to_f / 100
+   new_total = (self.total*discount).to_i
+         @total = new_total
+  pin_pad_reads = "After the discount, the total comes to $#{new_total}."
+  else 
+    pin_pad_reads = "There is no discount to apply."
+end
+return pin_pad_reads
+end 
+ 
+ def items 
+ return self.items
+ end 
+ 
+ def void_last_transaction
+   #!add_item(title,price)
+  # quantity-=1 
+  self.transaction = self.items.last #changed from [-1]
+  (self.total).to_i - (self.transaction).to_i
+  # @items.delete_at[-1]
+   if self.items.empty?
+     self.total = 0
+ end 
+ end 
+
+#def void_last_transaction
+  #  quantity = self.transaction[2]
+   # quantity.times do
+     # self.purchases.pop
+     # self.total -= self.transaction[1]
+   # end
+ # end
+
+##def add_item(name, price, quantity=1)
+   # self.transaction = [name, price, quantity]
+   # quantity.times do
+     # self.purchases.push self.transaction[0]
+     # self.total += self.transaction[1]
+   # end
+  #end
+
+
+ 
+ 
+ 
+ 
+ ##The void_last_transaction method will remove the last transaction from the total. You'll need to make an additional attribute accessor(last_transaction) and keep track of that last transaction amount somehow. In what method of the class are you working with an individual item?(add_item)
+ 
+ ##def add_item(title, price, quantity=1)
+#@total += price*quantity
+#quantity.to_i.times do 
+# @items << title
+
+#@last_transaction < remove from >> @items.to_i // @total
+#// @last_transaction = @items(-1) //
+#last_transaction = price*quantity
+#@items.delete_at(-1) 
+#end 
+#end
+ 
+ 
+end
